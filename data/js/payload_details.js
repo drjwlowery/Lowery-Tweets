ae4402d7129c93b80e77db71191ba069a489fca1
drjwlowery <jwlowery@gmail.com>
2019-10-13 21:13:28 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28218,
-	"created_at": "Sun Oct 13 01:13:27 +0000 2019",
+	"tweets": 28219,
+	"created_at": "Mon Oct 14 01:13:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sun Oct 13 16:36:00 +0000 2019",
+		"id": 1183421135351763000,
+		"id_str": "1183421135351762945",
+		"full_text": "Read more about Jackie Goldberg in her own words in this interview. She is on-top of the police car. https://t.co/6BRCkzExfI #HistSAHE60s https://t.co/nWIgqsJsER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			161
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HistSAHE60s",
+					"indices": [
+						125,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6BRCkzExfI",
+					"expanded_url": "http://ow.ly/1PpiYz",
+					"display_url": "ow.ly/1PpiYz",
+					"indices": [
+						101,
+						124
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1156232578359279600,
+					"id_str": "1156232578359279622",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/EAvDZ27WwAYcleE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EAvDZ27WwAYcleE.jpg",
+					"url": "https://t.co/nWIgqsJsER",
+					"display_url": "pic.twitter.com/nWIgqsJsER",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1156232583933571072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 498,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 500,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1156232583933571000,
+					"source_status_id_str": "1156232583933571072",
+					"source_user_id": 120855168,
+					"source_user_id_str": "120855168"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1156232578359279600,
+					"id_str": "1156232578359279622",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/EAvDZ27WwAYcleE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EAvDZ27WwAYcleE.jpg",
+					"url": "https://t.co/nWIgqsJsER",
+					"display_url": "pic.twitter.com/nWIgqsJsER",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1156232583933571072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 498,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 500,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1156232583933571000,
+					"source_status_id_str": "1156232583933571072",
+					"source_user_id": 120855168,
+					"source_user_id_str": "120855168"
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2181,
+			"friends_count": 1064,
+			"listed_count": 96,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 5556,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28312,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Read more about Jackie Goldberg in her own words in this interview. She is on-top of the police car. https://t.co/6BRCkzExfI #HistSAHE60s https://t.co/nWIgqsJsER"
+	},
+	{
 		"created_at": "Sat Oct 12 16:35:00 +0000 2019",
 		"id": 1183058495949561900,
 		"id_str": "1183058495949561857",
