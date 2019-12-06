edb3815cf2162a3e4b5e0c341742f1f1810228c2
drjwlowery <jwlowery@gmail.com>
2019-12-05 20:13:28 -0500

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,349 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 05 21:39:02 +0000 2019",
+		"id": 1202701428051107800,
+		"id_str": "1202703953424719877",
+		"full_text": "The Sands Hotel and Casino opened its doors on December 5, 1952. The Copa Room featured the Copa Girls who entertained guests. https://t.co/vSznNAY9uc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1196898232326140000,
+					"id_str": "1196898232326139906",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/EJw8mdIXsAItQ1A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJw8mdIXsAItQ1A.jpg",
+					"url": "https://t.co/vSznNAY9uc",
+					"display_url": "pic.twitter.com/vSznNAY9uc",
+					"expanded_url": "https://twitter.com/Nerd4Vegas/status/1202701428051107841/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 467,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1050,
+							"h": 721,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1050,
+							"h": 721,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 387,
+									"y": 97,
+									"h": 22,
+									"w": 22
+								},
+								{
+									"x": 424,
+									"y": 238,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 558,
+									"y": 95,
+									"h": 105,
+									"w": 105
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 599,
+									"y": 150,
+									"h": 34,
+									"w": 34
+								},
+								{
+									"x": 656,
+									"y": 369,
+									"h": 61,
+									"w": 61
+								},
+								{
+									"x": 862,
+									"y": 148,
+									"h": 163,
+									"w": 163
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 599,
+									"y": 150,
+									"h": 34,
+									"w": 34
+								},
+								{
+									"x": 656,
+									"y": 369,
+									"h": 61,
+									"w": 61
+								},
+								{
+									"x": 862,
+									"y": 148,
+									"h": 163,
+									"w": 163
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 599,
+									"y": 150,
+									"h": 34,
+									"w": 34
+								},
+								{
+									"x": 656,
+									"y": 369,
+									"h": 61,
+									"w": 61
+								},
+								{
+									"x": 862,
+									"y": 148,
+									"h": 163,
+									"w": 163
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1196898232326140000,
+					"id_str": "1196898232326139906",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/EJw8mdIXsAItQ1A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJw8mdIXsAItQ1A.jpg",
+					"url": "https://t.co/vSznNAY9uc",
+					"display_url": "pic.twitter.com/vSznNAY9uc",
+					"expanded_url": "https://twitter.com/Nerd4Vegas/status/1202701428051107841/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 467,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1050,
+							"h": 721,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1050,
+							"h": 721,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 387,
+									"y": 97,
+									"h": 22,
+									"w": 22
+								},
+								{
+									"x": 424,
+									"y": 238,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 558,
+									"y": 95,
+									"h": 105,
+									"w": 105
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 599,
+									"y": 150,
+									"h": 34,
+									"w": 34
+								},
+								{
+									"x": 656,
+									"y": 369,
+									"h": 61,
+									"w": 61
+								},
+								{
+									"x": 862,
+									"y": 148,
+									"h": 163,
+									"w": 163
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 599,
+									"y": 150,
+									"h": 34,
+									"w": 34
+								},
+								{
+									"x": 656,
+									"y": 369,
+									"h": 61,
+									"w": 61
+								},
+								{
+									"x": 862,
+									"y": 148,
+									"h": 163,
+									"w": 163
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 599,
+									"y": 150,
+									"h": 34,
+									"w": 34
+								},
+								{
+									"x": 656,
+									"y": 369,
+									"h": 61,
+									"w": 61
+								},
+								{
+									"x": 862,
+									"y": 148,
+									"h": 163,
+									"w": 163
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1165342315671634000,
+			"id_str": "1165342315671633920",
+			"name": "Nerd 4 Vegas",
+			"screen_name": "Nerd4Vegas",
+			"location": "Las Vegas",
+			"description": "Just sharing nerdy history and facts about the best city in the world!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38,
+			"friends_count": 35,
+			"listed_count": 0,
+			"created_at": "Sat Aug 24 19:17:21 +0000 2019",
+			"favourites_count": 94,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 37,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1165346868127289344/sHVXRis1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1165346868127289344/sHVXRis1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1165342315671633920/1566675320",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Sands Hotel and Casino opened its doors on December 5, 1952. The Copa Room featured the Copa Girls who entertained guests. https://t.co/vSznNAY9uc"
+	},
+	{
 		"created_at": "Wed Dec 04 20:53:11 +0000 2019",
 		"id": 1202330028752720000,
 		"id_str": "1202330028752719872",
