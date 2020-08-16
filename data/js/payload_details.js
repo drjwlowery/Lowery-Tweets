ac036cbea62bb8210749c0e76f0a7218df83ef6b
drjwlowery <jwlowery@gmail.com>
2020-08-15 21:13:29 -0400

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30019,
-	"created_at": "Sat Aug 15 01:13:28 +0000 2020",
+	"tweets": 30023,
+	"created_at": "Sun Aug 16 01:13:28 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,514 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 15 18:11:00 +0000 2020",
+		"id": 1294698117879734300,
+		"id_str": "1294698117879734272",
+		"full_text": "Delay of game? College football emerges as coronavirus flashpoint #HistSAHE https://t.co/Inbnc6xYbW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HistSAHE",
+					"indices": [
+						66,
+						75
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Inbnc6xYbW",
+					"expanded_url": "https://thehill.com/homenews/administration/512114-delay-of-game-college-football-emerges-as-coronavirus-flashpoint#.XzglFviubSg.twitter",
+					"display_url": "thehill.com/homenews/admin…",
+					"indices": [
+						76,
+						99
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2191,
+			"friends_count": 1164,
+			"listed_count": 93,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6283,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30362,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Delay of game? College football emerges as coronavirus flashpoint #HistSAHE https://t.co/Inbnc6xYbW"
+	},
+	{
+		"created_at": "Sat Aug 15 15:26:11 +0000 2020",
+		"id": 1294656642387193900,
+		"id_str": "1294656642387193856",
+		"full_text": "Screaming into the void! \n\nI spent 4-6 hours on Thursday setting up 90% of my D2L site for #HistSAHE and now (perhaps due to a D2L outage that started after I began work but had no effect on the functionality of D2L as I worked) it has disappeared! https://t.co/2ryDq844Ev",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			248
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HistSAHE",
+					"indices": [
+						91,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1294656510690295800,
+					"id_str": "1294656510690295810",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/EfeLPLHXgAI5X2b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfeLPLHXgAI5X2b.jpg",
+					"url": "https://t.co/2ryDq844Ev",
+					"display_url": "pic.twitter.com/2ryDq844Ev",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1294656642387193856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 244,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 460,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 431,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1294656510690295800,
+					"id_str": "1294656510690295810",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/EfeLPLHXgAI5X2b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfeLPLHXgAI5X2b.jpg",
+					"url": "https://t.co/2ryDq844Ev",
+					"display_url": "pic.twitter.com/2ryDq844Ev",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1294656642387193856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 244,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 460,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 431,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2191,
+			"friends_count": 1164,
+			"listed_count": 93,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6283,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30362,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Screaming into the void! \n\nI spent 4-6 hours on Thursday setting up 90% of my D2L site for #HistSAHE and now (perhaps due to a D2L outage that started after I began work but had no effect on the functionality of D2L as I worked) it has disappeared! https://t.co/2ryDq844Ev"
+	},
+	{
+		"created_at": "Sat Aug 15 15:17:45 +0000 2020",
+		"id": 1294277672877592600,
+		"id_str": "1294654516944687105",
+		"full_text": "NEW @EdSurge⁩ article! “It’s not a question of whether higher education institutions will see a significant uptick in burnout among staff, faculty and graduate students this fall. The more important question is how college leaders will address it.” https://t.co/XfPZm1AW0S",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EdSurge",
+					"name": "EdSurge",
+					"id": 274793711,
+					"id_str": "274793711",
+					"indices": [
+						4,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XfPZm1AW0S",
+					"expanded_url": "https://www.edsurge.com/news/2020-08-14-burnout-is-coming-to-campus-are-college-leaders-ready",
+					"display_url": "edsurge.com/news/2020-08-1…",
+					"indices": [
+						249,
+						272
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2397025926,
+			"id_str": "2397025926",
+			"name": "Kevin R. McClure",
+			"screen_name": "kevinrmcclure",
+			"location": "Wilmington, NC",
+			"description": "Assoc Prof of Higher Ed @UNCWilmington | Expert in College Management, Leadership, & Finance | Regional Public University Advocate | Program Coordinator",
+			"url": "https://t.co/g9hVLfMG5n",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/g9hVLfMG5n",
+							"expanded_url": "http://people.uncw.edu/mcclurek",
+							"display_url": "people.uncw.edu/mcclurek",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3497,
+			"friends_count": 1276,
+			"listed_count": 70,
+			"created_at": "Wed Mar 19 01:15:35 +0000 2014",
+			"favourites_count": 13577,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 14477,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1178714405036539906/FArZDbZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1178714405036539906/FArZDbZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2397025926/1593095417",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 82,
+		"favorite_count": 148,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "NEW @EdSurge⁩ article! “It’s not a question of whether higher education institutions will see a significant uptick in burnout among staff, faculty and graduate students this fall. The more important question is how college leaders will address it.” https://t.co/XfPZm1AW0S"
+	},
+	{
+		"created_at": "Sat Aug 15 14:05:53 +0000 2020",
+		"id": 1294390348731515000,
+		"id_str": "1294636434998071299",
+		"full_text": "A few days ago, the University of Chicago, i.e. the president and provost, emailed the campus with a subject mentioning “public safety”\n\nThe email starts out by calling for an “examination and reflection of law enforcement in our society”\n\n🧵 👇🏾",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 171753658,
+			"id_str": "171753658",
+			"name": "Damon Jones",
+			"screen_name": "nomadj1s",
+			"location": "Chicago, IL",
+			"description": "Faculty @HarrisPolicy | economist | from NJ | he/him",
+			"url": "https://t.co/GmGLhf9O2P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GmGLhf9O2P",
+							"expanded_url": "https://harris.uchicago.edu/directory/damon-jones",
+							"display_url": "harris.uchicago.edu/directory/damo…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 24570,
+			"friends_count": 1605,
+			"listed_count": 409,
+			"created_at": "Wed Jul 28 02:39:18 +0000 2010",
+			"favourites_count": 34046,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18213,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1248327354029690880/jlY_6njJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1248327354029690880/jlY_6njJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/171753658/1508857583",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1872,
+		"favorite_count": 4994,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "A few days ago, the University of Chicago, i.e. the president and provost, emailed the campus with a subject mentioning “public safety”\n\nThe email starts out by calling for an “examination and reflection of law enforcement in our society”\n\n🧵 👇🏾"
+	},
+	{
 		"created_at": "Fri Aug 14 21:15:13 +0000 2020",
 		"id": 1294382092042875000,
 		"id_str": "1294382092042874880",
