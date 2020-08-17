2211185cbc6c376d8b9551dc975272ebc2afef54
drjwlowery <jwlowery@gmail.com>
2020-08-16 21:13:29 -0400

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30023,
-	"created_at": "Sun Aug 16 01:13:28 +0000 2020",
+	"tweets": 30025,
+	"created_at": "Mon Aug 17 01:13:28 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sun Aug 16 03:55:24 +0000 2020",
+		"id": 1294845189652131800,
+		"id_str": "1294845189652131840",
+		"full_text": "DeVos’s controversial new rule on campus sexual assault goes into effect - The Washington Post #SAHElaw  https://t.co/WfHrE7A57r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SAHElaw",
+					"indices": [
+						95,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WfHrE7A57r",
+					"expanded_url": "https://www.washingtonpost.com/education/2020/08/14/betsy-devoss-controversial-new-rule-campus-sexual-assault-goes-into-effect/",
+					"display_url": "washingtonpost.com/education/2020…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2191,
+			"friends_count": 1164,
+			"listed_count": 93,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6289,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30365,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "DeVos’s controversial new rule on campus sexual assault goes into effect - The Washington Post #SAHElaw  https://t.co/WfHrE7A57r"
+	},
+	{
+		"created_at": "Sun Aug 16 03:00:39 +0000 2020",
+		"id": 1294831410323357700,
+		"id_str": "1294831410323357697",
+		"full_text": "@EricStoller @univmiami Reminds of this recent Notre Dame story where many of the recent cases were linked to a single party. https://t.co/Raow6hroEb",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EricStoller",
+					"name": "Eric Stoller",
+					"id": 1186911,
+					"id_str": "1186911",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "univmiami",
+					"name": "University of Miami",
+					"id": 15585350,
+					"id_str": "15585350",
+					"indices": [
+						13,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Raow6hroEb",
+					"expanded_url": "https://www.today.com/parents/notre-dame-sees-spike-covid-19-cases-after-early-reopening-t189502",
+					"display_url": "today.com/parents/notre-…",
+					"indices": [
+						126,
+						149
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1294601544399294500,
+		"in_reply_to_status_id_str": "1294601544399294466",
+		"in_reply_to_user_id": 1186911,
+		"in_reply_to_user_id_str": "1186911",
+		"in_reply_to_screen_name": "EricStoller",
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2191,
+			"friends_count": 1164,
+			"listed_count": 93,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6289,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30365,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@EricStoller @univmiami Reminds of this recent Notre Dame story where many of the recent cases were linked to a single party. https://t.co/Raow6hroEb"
+	},
+	{
 		"created_at": "Sat Aug 15 18:11:00 +0000 2020",
 		"id": 1294698117879734300,
 		"id_str": "1294698117879734272",
@@ -300,7 +541,7 @@ Grailbird.data.tweets_2020_08 =
 		"created_at": "Sat Aug 15 15:17:45 +0000 2020",
 		"id": 1294277672877592600,
 		"id_str": "1294654516944687105",
-		"full_text": "NEW @EdSurge⁩ article! “It’s not a question of whether higher education institutions will see a significant uptick in burnout among staff, faculty and graduate students this fall. The more important question is how college leaders will address it.” https://t.co/XfPZm1AW0S",
+		"full_text": "NEW @EdSurge article! “It’s not a question of whether higher education institutions will see a significant uptick in burnout among staff, faculty and graduate students this fall. The more important question is how college leaders will address it.” https://t.co/XfPZm1AW0S",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -411,7 +652,7 @@ Grailbird.data.tweets_2020_08 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "NEW @EdSurge⁩ article! “It’s not a question of whether higher education institutions will see a significant uptick in burnout among staff, faculty and graduate students this fall. The more important question is how college leaders will address it.” https://t.co/XfPZm1AW0S"
+		"text": "NEW @EdSurge article! “It’s not a question of whether higher education institutions will see a significant uptick in burnout among staff, faculty and graduate students this fall. The more important question is how college leaders will address it.” https://t.co/XfPZm1AW0S"
 	},
 	{
 		"created_at": "Sat Aug 15 14:05:53 +0000 2020",
