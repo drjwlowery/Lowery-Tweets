026a5d3b1fb143b00afb04eebb218916aa59c128
drjwlowery <jwlowery@gmail.com>
2020-09-24 21:13:30 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30273,
-	"created_at": "Thu Sep 24 01:13:30 +0000 2020",
+	"tweets": 30274,
+	"created_at": "Fri Sep 25 01:13:28 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 24 19:02:17 +0000 2020",
+		"id": 1309206539857801200,
+		"id_str": "1309206539857801221",
+		"full_text": "\"Working on a problem reduces the fear of it.\n\nIt’s hard to fear a problem when you are making progress on it—even if progress is imperfect and slow.\n\nAction relieves anxiety.\"\n\n–@JamesClear https://t.co/XKIux1Wh0U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JamesClear",
+					"name": "James Clear",
+					"id": 226428094,
+					"id_str": "226428094",
+					"indices": [
+						179,
+						190
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1309206530919719000,
+					"id_str": "1309206530919718913",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/Eis8Z0UXYAEApjJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eis8Z0UXYAEApjJ.jpg",
+					"url": "https://t.co/XKIux1Wh0U",
+					"display_url": "pic.twitter.com/XKIux1Wh0U",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1309206539857801221/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 580,
+							"h": 326,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 580,
+							"h": 326,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 580,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1309206530919719000,
+					"id_str": "1309206530919718913",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/Eis8Z0UXYAEApjJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eis8Z0UXYAEApjJ.jpg",
+					"url": "https://t.co/XKIux1Wh0U",
+					"display_url": "pic.twitter.com/XKIux1Wh0U",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1309206539857801221/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 580,
+							"h": 326,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 580,
+							"h": 326,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 580,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2196,
+			"friends_count": 1179,
+			"listed_count": 92,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6359,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30652,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Working on a problem reduces the fear of it.\n\nIt’s hard to fear a problem when you are making progress on it—even if progress is imperfect and slow.\n\nAction relieves anxiety.\"\n\n–@JamesClear https://t.co/XKIux1Wh0U"
+	},
+	{
 		"created_at": "Wed Sep 23 21:08:00 +0000 2020",
 		"id": 1308875787941171200,
 		"id_str": "1308875787941171203",
