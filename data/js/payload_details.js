935810d97483d9d75f21fb272c038f2f32cb8698
drjwlowery <jwlowery@gmail.com>
2020-11-14 20:13:27 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30441,
-	"created_at": "Sat Nov 14 01:13:27 +0000 2020",
+	"tweets": 30442,
+	"created_at": "Sun Nov 15 01:13:26 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sat Nov 14 18:58:00 +0000 2020",
+		"id": 1327687241557303300,
+		"id_str": "1327687241557303300",
+		"full_text": "60 years ago today in 1960, six-year-old Ruby Bridges was escorted into William Frantz Elementary School by a team of U.S. Deputy Marshals, desegregating the public school system of New Orleans. She was the only African-American student in the school. https://t.co/3wrXVTXV9C https://t.co/5EWrUe6rmy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3wrXVTXV9C",
+					"expanded_url": "https://pictorial.jezebel.com/today-is-the-anniversary-of-ruby-bridges-historic-first-1788949334?utm_campaign=socialflow_jezebel_facebook&utm_source=jezebel_facebook&utm_medium=socialflow",
+					"display_url": "pictorial.jezebel.com/today-is-the-a…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1197222024957046800,
+					"id_str": "1197222024957046789",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ1jFqrWsAUTpeu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJ1jFqrWsAUTpeu.jpg",
+					"url": "https://t.co/5EWrUe6rmy",
+					"display_url": "pic.twitter.com/5EWrUe6rmy",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1327687241557303300/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197222024957046800,
+					"id_str": "1197222024957046789",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ1jFqrWsAUTpeu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EJ1jFqrWsAUTpeu.jpg",
+					"url": "https://t.co/5EWrUe6rmy",
+					"display_url": "pic.twitter.com/5EWrUe6rmy",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1327687241557303300/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2182,
+			"friends_count": 1181,
+			"listed_count": 92,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6386,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30852,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "60 years ago today in 1960, six-year-old Ruby Bridges was escorted into William Frantz Elementary School by a team of U.S. Deputy Marshals, desegregating the public school system of New Orleans. She was the only African-American student in the school. https://t.co/3wrXVTXV9C https://t.co/5EWrUe6rmy"
+	},
+	{
 		"created_at": "Sat Nov 14 00:00:27 +0000 2020",
 		"id": 1327400970104483800,
 		"id_str": "1327400970104483842",
