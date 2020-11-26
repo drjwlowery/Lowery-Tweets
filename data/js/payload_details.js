6e36d059d1bd355ef43c3e8844d0eca8470a40eb
drjwlowery <jwlowery@gmail.com>
2020-11-25 20:13:29 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30478,
-	"created_at": "Wed Nov 25 01:13:27 +0000 2020",
+	"tweets": 30479,
+	"created_at": "Thu Nov 26 01:13:28 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,278 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 25 23:47:56 +0000 2020",
+		"id": 1331746471243296800,
+		"id_str": "1331746471243296771",
+		"full_text": "#HistSAHE https://t.co/KeuW6yPk2y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HistSAHE",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/KeuW6yPk2y",
+					"expanded_url": "https://twitter.com/eddiercole/status/1331623343938789378",
+					"display_url": "twitter.com/eddiercole/sta…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2182,
+			"friends_count": 1185,
+			"listed_count": 91,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30897,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1331623343938789400,
+		"quoted_status_id_str": "1331623343938789378",
+		"quoted_status_permalink": {
+			"url": "https://t.co/KeuW6yPk2y",
+			"expanded": "https://twitter.com/eddiercole/status/1331623343938789378",
+			"display": "twitter.com/eddiercole/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 25 15:38:40 +0000 2020",
+			"id": 1331623343938789400,
+			"id_str": "1331623343938789378",
+			"full_text": "My latest — now in the @chronicle.\n\nOn race and #HigherEd history — and how past actions (and inactions) guide today.\n\n@ChronicleReview #CampusColorLine\nhttps://t.co/UtPIq2fzCK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				176
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "HigherEd",
+						"indices": [
+							48,
+							57
+						]
+					},
+					{
+						"text": "CampusColorLine",
+						"indices": [
+							136,
+							152
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "chronicle",
+						"name": "The Chronicle of Higher Education",
+						"id": 12413032,
+						"id_str": "12413032",
+						"indices": [
+							23,
+							33
+						]
+					},
+					{
+						"screen_name": "ChronicleReview",
+						"name": "Chronicle Review",
+						"id": 900154572,
+						"id_str": "900154572",
+						"indices": [
+							119,
+							135
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/UtPIq2fzCK",
+						"expanded_url": "https://www.chronicle.com/article/academes-disturbing-indifference-to-racism?cid2=gen_login_refresh&cid=gen_sign_in",
+						"display_url": "chronicle.com/article/academ…",
+						"indices": [
+							153,
+							176
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 560850949,
+				"id_str": "560850949",
+				"name": "Eddie R. Cole",
+				"screen_name": "EddieRCole",
+				"location": "Los Angeles, CA",
+				"description": "professor: @ucla. author: 𝙏𝙝𝙚 𝘾𝙖𝙢𝙥𝙪𝙨 𝘾𝙤𝙡𝙤𝙧 𝙇𝙞𝙣𝙚. Now available on @PrincetonUPress. Order here: https://t.co/3WwzpIt3fW",
+				"url": "https://t.co/WzifREoEWD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WzifREoEWD",
+								"expanded_url": "http://www.eddiercole.com",
+								"display_url": "eddiercole.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/3WwzpIt3fW",
+								"expanded_url": "https://tinyurl.com/CampusColorLine",
+								"display_url": "tinyurl.com/CampusColorLine",
+								"indices": [
+									96,
+									119
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1681,
+				"friends_count": 352,
+				"listed_count": 43,
+				"created_at": "Mon Apr 23 05:22:32 +0000 2012",
+				"favourites_count": 5248,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5413,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1294261367596343301/um628DFT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1294261367596343301/um628DFT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/560850949/1591127434",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 16,
+			"favorite_count": 44,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#HistSAHE https://t.co/KeuW6yPk2y"
+	},
+	{
 		"created_at": "Tue Nov 24 13:43:58 +0000 2020",
 		"id": 1331232091980361700,
 		"id_str": "1331232091980361730",
