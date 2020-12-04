af3c80c51c363c188f18d932f6ddbc3cfc1b0623
drjwlowery <jwlowery@gmail.com>
2020-12-03 20:13:27 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30495,
-	"created_at": "Thu Dec 03 01:13:26 +0000 2020",
+	"tweets": 30496,
+	"created_at": "Fri Dec 04 01:13:26 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 03 21:50:28 +0000 2020",
+		"id": 1334616015414685700,
+		"id_str": "1334616015414685698",
+		"full_text": "\"The secret to winning is learning how to lose.\n\nThat is, learning to bounce back from failure and disappointment—undeterred—and continuing to steadily march toward your potential.\n\nYour response to failure determines your capacity for success.\"\n\n-@JamesClear https://t.co/iUIRZxuLb8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JamesClear",
+					"name": "James Clear",
+					"id": 226428094,
+					"id_str": "226428094",
+					"indices": [
+						248,
+						259
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1334543068448645000,
+					"id_str": "1334543068448645123",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/EoU_2ZxXYAMI4AE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoU_2ZxXYAMI4AE.jpg",
+					"url": "https://t.co/iUIRZxuLb8",
+					"display_url": "pic.twitter.com/iUIRZxuLb8",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1334616015414685698/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 451,
+							"h": 254,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 451,
+							"h": 254,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 451,
+							"h": 254,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334543068448645000,
+					"id_str": "1334543068448645123",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/EoU_2ZxXYAMI4AE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoU_2ZxXYAMI4AE.jpg",
+					"url": "https://t.co/iUIRZxuLb8",
+					"display_url": "pic.twitter.com/iUIRZxuLb8",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1334616015414685698/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 451,
+							"h": 254,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 451,
+							"h": 254,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 451,
+							"h": 254,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2183,
+			"friends_count": 1190,
+			"listed_count": 91,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6426,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 30920,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"The secret to winning is learning how to lose.\n\nThat is, learning to bounce back from failure and disappointment—undeterred—and continuing to steadily march toward your potential.\n\nYour response to failure determines your capacity for success.\"\n\n-@JamesClear https://t.co/iUIRZxuLb8"
+	},
+	{
 		"created_at": "Wed Dec 02 20:21:55 +0000 2020",
 		"id": 1334231341613854700,
 		"id_str": "1334231341613854721",
