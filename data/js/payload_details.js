42e5fd46a75198e1c3bc596f2ea35e753d3edb37
drjwlowery <jwlowery@gmail.com>
2021-08-11 21:13:33 -0400

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31749,
-	"created_at": "Wed Aug 11 01:13:26 +0000 2021",
+	"tweets": 31752,
+	"created_at": "Thu Aug 12 01:13:31 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,425 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 12 00:35:47 +0000 2021",
+		"id": 1425616971140407300,
+		"id_str": "1425616971140407297",
+		"full_text": "Can Biden Cancel Student Debt? It Depends Who You Ask\n\nLegal analyses of the same provision in the Higher Education Act come to different conclusions as to whether the administration has the authority to unilaterally cancel student loan debt. https://t.co/qd9BRefGWN #SAHElaw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SAHElaw",
+					"indices": [
+						267,
+						275
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qd9BRefGWN",
+					"expanded_url": "https://www.insidehighered.com/news/2021/08/11/legal-experts-are-split-whether-biden-can-cancel-debt#.YRRstmzfrJI.twitter",
+					"display_url": "insidehighered.com/news/2021/08/1…",
+					"indices": [
+						243,
+						266
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2169,
+			"friends_count": 1294,
+			"listed_count": 94,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6581,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32377,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Can Biden Cancel Student Debt? It Depends Who You Ask\n\nLegal analyses of the same provision in the Higher Education Act come to different conclusions as to whether the administration has the authority to unilaterally cancel student loan debt. https://t.co/qd9BRefGWN #SAHElaw"
+	},
+	{
+		"created_at": "Wed Aug 11 23:36:51 +0000 2021",
+		"id": 1425464168413470700,
+		"id_str": "1425602142807678983",
+		"full_text": "Hold the dates. #UNT #Texas #HigherEducation Law Conference Nov 1-3. #UNTedu We’ll have an excellent array of presenters on #TitleIX #NCAA #DACA #freespeech #socialmedia #FERPA #StudentAffairs #communitycollege #internationalstudents #COVID #vaccinemandate &amp; more. Please RT.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "UNT",
+					"indices": [
+						16,
+						20
+					]
+				},
+				{
+					"text": "Texas",
+					"indices": [
+						21,
+						27
+					]
+				},
+				{
+					"text": "HigherEducation",
+					"indices": [
+						28,
+						44
+					]
+				},
+				{
+					"text": "UNTedu",
+					"indices": [
+						69,
+						76
+					]
+				},
+				{
+					"text": "TitleIX",
+					"indices": [
+						124,
+						132
+					]
+				},
+				{
+					"text": "NCAA",
+					"indices": [
+						133,
+						138
+					]
+				},
+				{
+					"text": "DACA",
+					"indices": [
+						139,
+						144
+					]
+				},
+				{
+					"text": "freespeech",
+					"indices": [
+						145,
+						156
+					]
+				},
+				{
+					"text": "socialmedia",
+					"indices": [
+						157,
+						169
+					]
+				},
+				{
+					"text": "FERPA",
+					"indices": [
+						170,
+						176
+					]
+				},
+				{
+					"text": "StudentAffairs",
+					"indices": [
+						177,
+						192
+					]
+				},
+				{
+					"text": "communitycollege",
+					"indices": [
+						193,
+						210
+					]
+				},
+				{
+					"text": "internationalstudents",
+					"indices": [
+						211,
+						233
+					]
+				},
+				{
+					"text": "COVID",
+					"indices": [
+						234,
+						240
+					]
+				},
+				{
+					"text": "vaccinemandate",
+					"indices": [
+						241,
+						256
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 931292916248608800,
+			"id_str": "931292916248608768",
+			"name": "Cliff Harbour",
+			"screen_name": "Cliff_Harbour",
+			"location": "Denton, Texas, USA",
+			"description": "Higher Education Professor, Community Colleges, Dewey, Sustainable Campuses... Born CO2 level @ 315 Mauna Loa. Throws right, bats left. Views are my own.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 457,
+			"friends_count": 724,
+			"listed_count": 8,
+			"created_at": "Thu Nov 16 22:48:28 +0000 2017",
+			"favourites_count": 3839,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 873,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/969228777556512769/vXTRZi3A_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/969228777556512769/vXTRZi3A_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/931292916248608768/1513192309",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Hold the dates. #UNT #Texas #HigherEducation Law Conference Nov 1-3. #UNTedu We’ll have an excellent array of presenters on #TitleIX #NCAA #DACA #freespeech #socialmedia #FERPA #StudentAffairs #communitycollege #internationalstudents #COVID #vaccinemandate &amp; more. Please RT."
+	},
+	{
+		"created_at": "Wed Aug 11 23:36:08 +0000 2021",
+		"id": 1425601960267354000,
+		"id_str": "1425601960267354115",
+		"full_text": "DOJ, Brown University settle case on readmitting students treated for mental issues | TheHill #SAHElaw  https://t.co/0LiqeVd7ZY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SAHElaw",
+					"indices": [
+						94,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0LiqeVd7ZY",
+					"expanded_url": "https://thehill.com/regulation/court-battles/567395-doj-brown-university-settle-case-on-readmitting-students-treated-for",
+					"display_url": "thehill.com/regulation/cou…",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2169,
+			"friends_count": 1294,
+			"listed_count": 94,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6581,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32377,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "DOJ, Brown University settle case on readmitting students treated for mental issues | TheHill #SAHElaw  https://t.co/0LiqeVd7ZY"
+	},
+	{
 		"created_at": "Tue Aug 10 13:42:01 +0000 2021",
 		"id": 1425090057862529000,
 		"id_str": "1425090057862529028",
