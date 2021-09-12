3cce9319451727c8fa2c851b2f795e55a8ddbebb
drjwlowery <jwlowery@gmail.com>
2021-09-11 21:13:27 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31898,
-	"created_at": "Sat Sep 11 01:13:27 +0000 2021",
+	"tweets": 31899,
+	"created_at": "Sun Sep 12 01:13:26 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 11 13:33:16 +0000 2021",
+		"id": 1436684270026821600,
+		"id_str": "1436684270026821632",
+		"full_text": "#OnThisDay in 1963, Henrie Monteith (now Treadwell), Robert Anderson, and James Solomon enrolled in University of South Carolina, the first African American students to do so since Reconstruction. #HistSAHE \n\nPosted to FB by USC Center for Civil Rights History and Research https://t.co/XO7vLhOImk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OnThisDay",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "HistSAHE",
+					"indices": [
+						197,
+						206
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1436684256034664400,
+					"id_str": "1436684256034664448",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/E_AguA9XsAAZa7W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_AguA9XsAAZa7W.jpg",
+					"url": "https://t.co/XO7vLhOImk",
+					"display_url": "pic.twitter.com/XO7vLhOImk",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1436684270026821632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 472,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 592,
+							"h": 852,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 592,
+							"h": 852,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1436684256034664400,
+					"id_str": "1436684256034664448",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/E_AguA9XsAAZa7W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_AguA9XsAAZa7W.jpg",
+					"url": "https://t.co/XO7vLhOImk",
+					"display_url": "pic.twitter.com/XO7vLhOImk",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1436684270026821632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 472,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 592,
+							"h": 852,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 592,
+							"h": 852,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1436684256093343700,
+					"id_str": "1436684256093343745",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/E_AguBLXEAEiuVL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_AguBLXEAEiuVL.jpg",
+					"url": "https://t.co/XO7vLhOImk",
+					"display_url": "pic.twitter.com/XO7vLhOImk",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1436684270026821632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 571,
+							"h": 858,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 571,
+							"h": 858,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1436684256076513300,
+					"id_str": "1436684256076513293",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/E_AguBHWQA07xQo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_AguBHWQA07xQo.jpg",
+					"url": "https://t.co/XO7vLhOImk",
+					"display_url": "pic.twitter.com/XO7vLhOImk",
+					"expanded_url": "https://twitter.com/drjwlowery/status/1436684270026821632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 573,
+							"h": 850,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 573,
+							"h": 850,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 458,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2174,
+			"friends_count": 1304,
+			"listed_count": 93,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6601,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32549,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#OnThisDay in 1963, Henrie Monteith (now Treadwell), Robert Anderson, and James Solomon enrolled in University of South Carolina, the first African American students to do so since Reconstruction. #HistSAHE \n\nPosted to FB by USC Center for Civil Rights History and Research https://t.co/XO7vLhOImk"
+	},
+	{
 		"created_at": "Fri Sep 10 19:56:22 +0000 2021",
 		"id": 1436418290750996500,
 		"id_str": "1436418290750996485",
