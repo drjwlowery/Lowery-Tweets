d8f178027117fe040273f43914575130e714de70
drjwlowery <jwlowery@gmail.com>
2021-09-18 21:13:28 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31931,
-	"created_at": "Fri Sep 17 01:13:27 +0000 2021",
+	"tweets": 31932,
+	"created_at": "Sun Sep 19 01:13:27 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 18 16:16:27 +0000 2021",
+		"id": 1439262049968668700,
+		"id_str": "1439262049968668675",
+		"full_text": "@MichaelHevel @RHE_ASHE @universityofga @NAEduc @Spencer_Fdn @uarkhied @UGA_IHE @LGBTRCatUGA @UofA_Research @uacoehp Great! I look forward to reading this. I am familiar with one of their successful lawsuits, but not the larger story.",
+		"truncated": false,
+		"display_text_range": [
+			117,
+			234
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MichaelHevel",
+					"name": "Michael Hevel",
+					"id": 422058422,
+					"id_str": "422058422",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "RHE_ASHE",
+					"name": "Review of Higher Education",
+					"id": 1199376542809374700,
+					"id_str": "1199376542809374720",
+					"indices": [
+						14,
+						23
+					]
+				},
+				{
+					"screen_name": "universityofga",
+					"name": "UGA",
+					"id": 16435792,
+					"id_str": "16435792",
+					"indices": [
+						24,
+						39
+					]
+				},
+				{
+					"screen_name": "NAEduc",
+					"name": "NAEd",
+					"id": 2648804574,
+					"id_str": "2648804574",
+					"indices": [
+						40,
+						47
+					]
+				},
+				{
+					"screen_name": "Spencer_Fdn",
+					"name": "Spencer Foundation",
+					"id": 906219834081509400,
+					"id_str": "906219834081509377",
+					"indices": [
+						48,
+						60
+					]
+				},
+				{
+					"screen_name": "uarkhied",
+					"name": "University of Arkansas Higher Education Program",
+					"id": 2805769352,
+					"id_str": "2805769352",
+					"indices": [
+						61,
+						70
+					]
+				},
+				{
+					"screen_name": "UGA_IHE",
+					"name": "UGA IHE",
+					"id": 2602081890,
+					"id_str": "2602081890",
+					"indices": [
+						71,
+						79
+					]
+				},
+				{
+					"screen_name": "LGBTRCatUGA",
+					"name": "UGA LGBTRC",
+					"id": 1209033936,
+					"id_str": "1209033936",
+					"indices": [
+						80,
+						92
+					]
+				},
+				{
+					"screen_name": "UofA_Research",
+					"name": "UofA Division of Research and Innovation",
+					"id": 2350279279,
+					"id_str": "2350279279",
+					"indices": [
+						93,
+						107
+					]
+				},
+				{
+					"screen_name": "uacoehp",
+					"name": "UA COEHP",
+					"id": 2361771324,
+					"id_str": "2361771324",
+					"indices": [
+						108,
+						116
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1438880689764528000,
+		"in_reply_to_status_id_str": "1438880689764528138",
+		"in_reply_to_user_id": 422058422,
+		"in_reply_to_user_id_str": "422058422",
+		"in_reply_to_screen_name": "MichaelHevel",
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2175,
+			"friends_count": 1306,
+			"listed_count": 93,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6614,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32586,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@MichaelHevel @RHE_ASHE @universityofga @NAEduc @Spencer_Fdn @uarkhied @UGA_IHE @LGBTRCatUGA @UofA_Research @uacoehp Great! I look forward to reading this. I am familiar with one of their successful lawsuits, but not the larger story."
+	},
+	{
 		"created_at": "Thu Sep 16 19:19:00 +0000 2021",
 		"id": 1438583213111353300,
 		"id_str": "1438583213111353345",
