3b001bc91df885853d07d3cd9122a544130991ec
drjwlowery <jwlowery@gmail.com>
2021-12-31 20:13:27 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32318,
-	"created_at": "Fri Dec 31 01:13:27 +0000 2021",
+	"tweets": 32320,
+	"created_at": "Sat Jan 01 01:13:26 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Fri Dec 31 15:29:23 +0000 2021",
+		"id": 1476938541783519200,
+		"id_str": "1476938541783519271",
+		"full_text": "Should Prominent Surgeon Be Able to Pseudonymously Sue University of Michigan for Allegedly Improper Suspension? #SAHElaw  https://t.co/TfchWgadOT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			146
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SAHElaw",
+					"indices": [
+						113,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TfchWgadOT",
+					"expanded_url": "https://reason.com/volokh/2021/12/30/should-prominent-surgeon-be-able-to-pseudonymously-sue-university-of-michigan-for-allegedly-improper-suspension/",
+					"display_url": "reason.com/volokh/2021/12…",
+					"indices": [
+						123,
+						146
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120855168,
+			"id_str": "120855168",
+			"name": "John Wesley Lowery",
+			"screen_name": "drjwlowery",
+			"location": "Indiana, PA",
+			"description": "IUP SAHE Prof & Chair teaching & researching in areas of student affairs history, law, & cont. issues. Rock-star in a bow tie. RT ≠ endorsement or agreement",
+			"url": "https://t.co/A3oM0uSiGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/A3oM0uSiGi",
+							"expanded_url": "http://www.johnwesleylowery.com",
+							"display_url": "johnwesleylowery.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2174,
+			"friends_count": 1325,
+			"listed_count": 0,
+			"created_at": "Sun Mar 07 19:41:09 +0000 2010",
+			"favourites_count": 6850,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 33047,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/728991630942994434/Pz_8YYc-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120855168/1398374728",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Should Prominent Surgeon Be Able to Pseudonymously Sue University of Michigan for Allegedly Improper Suspension? #SAHElaw  https://t.co/TfchWgadOT"
+	},
+	{
+		"created_at": "Fri Dec 31 04:30:37 +0000 2021",
+		"id": 1476401574763184000,
+		"id_str": "1476772759573340169",
+		"full_text": "Today I learned that there are five types of rest:\n\n1. Sleep\n2. Solitude - doing stuff on your own\n3. Stillness - doing nothing\n4. Being in nature\n5. Being with people with whom you share love\n\nGet a balanced diet! Of the five types of rest!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			241
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2317921,
+			"id_str": "2317921",
+			"name": "John Moe",
+			"screen_name": "johnmoe",
+			"location": "St Paul, MN",
+			"description": "Host of DEPRESH MODE. Author-The Hilarious World of Depression, other books. Podcaster, public speaker. a website: https://t.co/LqK6EjcsGa",
+			"url": "https://t.co/XkP3MSk2E2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XkP3MSk2E2",
+							"expanded_url": "https://maximumfun.org/podcasts/depresh-mode/",
+							"display_url": "maximumfun.org/podcasts/depre…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/LqK6EjcsGa",
+							"expanded_url": "http://johnmoe.website",
+							"display_url": "johnmoe.website",
+							"indices": [
+								115,
+								138
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 42902,
+			"friends_count": 1016,
+			"listed_count": 1226,
+			"created_at": "Mon Mar 26 16:11:51 +0000 2007",
+			"favourites_count": 34356,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 102882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1371511559500001283/3zyfrmVL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1371511559500001283/3zyfrmVL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2317921/1567266650",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 121,
+		"favorite_count": 734,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Today I learned that there are five types of rest:\n\n1. Sleep\n2. Solitude - doing stuff on your own\n3. Stillness - doing nothing\n4. Being in nature\n5. Being with people with whom you share love\n\nGet a balanced diet! Of the five types of rest!"
+	},
+	{
 		"created_at": "Thu Dec 30 22:34:13 +0000 2021",
 		"id": 1476683069037990000,
 		"id_str": "1476683069037989891",
